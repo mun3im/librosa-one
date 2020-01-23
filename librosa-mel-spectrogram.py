b0ddd9cf97259fddffefa13c53950cee9a3bbb11
Munim Zabidi <mzabidi@gmail.com>
2020-01-23 14:27:43 +0800

    iniital commit

---
+++ librosa-mel-spectrogram.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+#
+# Getting to Know the Mel Spectrogram
+# https://towardsdatascience.com/getting-to-know-the-mel-spectrogram-31bca3e2d9d0
+#
+#from numpy import loadtxt
+#from keras.models import Sequential
+#from keras.layers import Dense
+#import os
+#os.chdir('/home/munim/first-keras')
+#print(os.getcwd())
+
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+filename = 'humpback_whales.wav'
+y, sr = librosa.load(filename)
+# trim silent edges
+whale_song, _ = librosa.effects.trim(y)
+# plot time domain
+librosa.display.waveplot(whale_song, sr=sr);
+
+# plot stft one short windows
+n_fft = 2048
+D = np.abs(librosa.stft(whale_song[:n_fft], n_fft=n_fft, hop_length=n_fft+1))
+plt.plot(D);
+
+#plot linear spectrogram
+hop_length = 512
+D = np.abs(librosa.stft(whale_song, n_fft=n_fft, hop_length=hop_length))
+librosa.display.specshow(D, sr=sr, x_axis='time', y_axis='linear');
+plt.colorbar();
+
+#plot log spectrogram
+DB = librosa.amplitude_to_db(D, ref=np.max)
+librosa.display.specshow(DB, sr=sr, hop_length=hop_length, 
+                         x_axis='time', y_axis='log');
+plt.colorbar(format='%+2.0f dB');
+
+n_mels = 128
+mel = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=n_mels)
+
+plt.figure(figsize=(15, 4));
+plt.subplot(1, 3, 1);
+librosa.display.specshow(mel, sr=sr, hop_length=hop_length, 
+                         x_axis='linear');
+plt.ylabel('Mel filter');
+plt.colorbar();
+plt.title('1. Our filter bank for converting from Hz to mels.');
+plt.subplot(1, 3, 2);
+mel_10 = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=10)
+librosa.display.specshow(mel_10, sr=sr, hop_length=hop_length, 
+                         x_axis='linear');
+plt.ylabel('Mel filter');
+plt.colorbar();
+plt.title('2. Easier to see what is happening with only 10 mels.');
+plt.subplot(1, 3, 3);
+idxs_to_plot = [0, 9, 49, 99, 127]
+for i in idxs_to_plot:
+    plt.plot(mel[i]);
+plt.legend(labels=['{}'.format(i+1) for i in idxs_to_plot]);
+plt.title('3. Plotting some triangular filters separately.');
+plt.tight_layout();
+
+S = librosa.feature.melspectrogram(whale_song, sr=sr, n_fft=n_fft, 
+                                   hop_length=hop_length, 
+                                   n_mels=n_mels)
+S_DB = librosa.power_to_db(S, ref=np.max)
+librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length, 
+                         x_axis='time', y_axis='mel');
+plt.colorbar(format='%+2.0f dB');
+
+# Sanity check that indeed we understood the underlying pipeline
+S = librosa.feature.melspectrogram(whale_song, sr=sr, n_fft=n_fft, 
+                                   hop_length=hop_length, 
+                                   n_mels=n_mels)
+fft_windows = librosa.stft(whale_song, n_fft=n_fft, 
+                           hop_length=hop_length)
+magnitude = np.abs(fft_windows)**2
+mel = librosa.filters.mel(sr=sr, n_fft=n_fft, n_mels=n_mels)
+assert (mel.dot(magnitude) == S).all()
+
+
+
